aee5124de310a8cad840b41943b1c9487fba9e3d
Jason Wang <jasonxwang9@gmail.com>
2025-04-12 19:21:47 -0400

    fix tailwind bug

--- my-project/preload.js
+++ my-project/preload.js
@@ -1,5 +1,5 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electronAPI', {
-  selectVideo: () => ipcRenderer.invoke('select-video')
+    selectVideo: () => ipcRenderer.invoke('select-video'),
 });
