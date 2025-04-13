d5b09e869f48e1f462ed0a0b6c65a3906720f459
Jace <jacerred@gmail.com>
2025-04-12 23:08:49 -0400

    things

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,8 +1,9 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Upload from './components/Upload';
 import Page from './components/Page';
 import NavBar from './components/Navbar';
+import Main from './pages/Main';
+import { Datepicker } from "flowbite-react";
 
 function App() {
   return (
@@ -13,10 +14,18 @@ function App() {
       <div className='bg-gray-900 text-white'>
         <Router>
           <NavBar />
-          <Routes>
-            <Route path='/' element={< Upload />}/>
-            <Route path='/journal' element= {< Page />}/>
-          </Routes>
+          <div className='flex'>
+            <div className='h-full'>
+              <Datepicker/>
+            </div>
+            <div className='w-full h-full'>
+              <Routes>
+                <Route path='/' element={< Main />}/>
+                <Route path='/journal' element= {< Page />}/>
+              </Routes>
+            </div>
+          </div>
+          
         </Router>
       </div>
     </>
--- my-project/tailwind.config.js
+++ my-project/tailwind.config.js
@@ -1,14 +1,12 @@
+const flowbiteReact = require("flowbite-react/plugin/tailwindcss");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-    "./node_modules/flowbite-react/**/*.js", // for Flowbite React
-    "./node_modules/flowbite/**/*.js", // for base Flowbite
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", // for Flowbite React
+  "./node_modules/flowbite-react/**/*.js", // for base Flowbite
+  "./node_modules/flowbite/**/*.js", ".flowbite-react\\class-list.json"],
   theme: {
     extend: {},
   },
-  plugins: [
-    require('flowbite/plugin')
-  ],
+  plugins: [require('flowbite/plugin'), flowbiteReact],
 }
